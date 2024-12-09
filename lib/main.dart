a852f02a7cfb8fce3ea19e01e34621dd34fdbc79
ks-shinjuku02 <ks-shinjuku02>
2024-12-09 09:58:44 +0800

    2

--- lib/main.dart
+++ lib/main.dart
@@ -108,6 +108,7 @@ class _MyHomePageState extends State<MyHomePage> {
             const Text(
               'You have pushed the button this many times:',
             ),
+            Container(color: Colors.red),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
